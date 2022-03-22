470b71ea689acebf171eb7481ee75aefeb78a1b4
br3nds0n <brendson.net@gmail.com>
2022-03-22 08:42:14 -0300

    feat: create route delete

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 
 import { CreateCategoryController } from '../app/controllers/CreateCategoryController';
+import { DeleteCategoryController } from '../app/controllers/DeleteCategoryController';
 import { GetAllCategoriesController } from '../app/controllers/GetAllCategoriesController';
 
 const routes = Router();
 
 routes
 	.post('/categorias', new CreateCategoryController().handle)
-	.get('/categorias', new GetAllCategoriesController().handle);
-
+	.get('/categorias', new GetAllCategoriesController().handle)
+	.delete('/categorias/:id', new DeleteCategoryController().handle);
 
 export { routes };
