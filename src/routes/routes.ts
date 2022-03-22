fe0fd844aa1762eb256652083bbc973a9dea6fdb
br3nds0n <brendson.net@gmail.com>
2022-03-22 09:08:11 -0300

    feat: create route put

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,12 +3,14 @@ import { Router } from 'express';
 import { CreateCategoryController } from '../app/controllers/CreateCategoryController';
 import { DeleteCategoryController } from '../app/controllers/DeleteCategoryController';
 import { GetAllCategoriesController } from '../app/controllers/GetAllCategoriesController';
+import { UpdateCategoryController } from '../app/controllers/UpdateCategoryController';
 
 const routes = Router();
 
 routes
 	.post('/categorias', new CreateCategoryController().handle)
 	.get('/categorias', new GetAllCategoriesController().handle)
-	.delete('/categorias/:id', new DeleteCategoryController().handle);
+	.delete('/categorias/:id', new DeleteCategoryController().handle)
+	.put('/categorias/:id', new UpdateCategoryController().handle);
 
 export { routes };
